81e10f5c7b94b9125407ab567b638db65db8aeed
nkp <nkp216@gmail.com>
2020-03-30 15:55:49 +0300

    added ge

---
+++ ge/gui.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+#maximize window
+root_window.attributes("-zoomed",1)
+
+
+#The following stops the "Do you really want to quit" dialog.
+root_window.tk.call("wm","protocol",".","WM_DELETE_WINDOW","destroy .")
+
+#set start_view  view_z
+#commands.set_view_z()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
